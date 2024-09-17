2eaa9d12444c569214e4a27a973cf151f4cdbb17
Garrick Wong <inquisitor8986@gmail.com>
2024-09-16 22:10:41 -0600

    Added some things to the GUI.

--- gui.py
+++ gui.py
@@ -4,3 +4,16 @@ import wx
 class guiFream(wx.Frame):
     def __init__(self, *args, **kwargs):
         panel = wx.Panel(self)
+        self.InitUI(panel)
+        self.label = wx.StaticText(panel, label = "Choose a password length: ", pos = )
+        self.info = 0
+
+    def InitUI(self, panel):
+        self.getLength(panel)
+        self.SetSize((250,250))
+        self.SetTitle('Password Generator')
+        self.Center()
+
+    def getLength(self, panel):
+        self.text_ctrl = wx.TextCtrl(panel, pos = (75, 75))
+        self.button = wx.Button(panel, label = "Create Password")
