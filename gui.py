841fe49e68f71dc80730ea3853db6960d9642993
Garrick Wong <inquisitor8986@gmail.com>
2024-09-17 21:34:28 -0600

    Small things

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ class guiFream(wx.Frame):
     def __init__(self, *args, **kwargs):
         panel = wx.Panel(self)
         self.InitUI(panel)
-        self.label = wx.StaticText(panel, label = "Choose a password length: ", pos = )
+        self.label = wx.StaticText(panel, label = "Choose a password length: ", pos = (15,15))
         self.info = 0
 
     def InitUI(self, panel):
@@ -17,3 +17,6 @@ class guiFream(wx.Frame):
     def getLength(self, panel):
         self.text_ctrl = wx.TextCtrl(panel, pos = (75, 75))
         self.button = wx.Button(panel, label = "Create Password")
+
+    def buttonTriggered(self, panel):
+        
